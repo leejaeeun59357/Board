4cdc6cfa4ecd1277775757ba2d45e9711c9a64e9
Leejaeeun <ne2227@naver.com>
2024-03-17 00:23:31 +0900

    feat: [008-LogIn/LogOut] 로그인, 로그아웃 기능 구현

--- src/main/java/org/mysite/sbb/user/UserRepository.java
+++ src/main/java/org/mysite/sbb/user/UserRepository.java
@@ -3,6 +3,9 @@ package org.mysite.sbb.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<SiteUser,Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
